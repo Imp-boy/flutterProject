2149e0294757abfa4cc0bc1f2b1343a88c986601
Imp-boy <1126434179@qq.com>
2019-05-22 08:20:50 +0800

    测试提交

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
-
+//无状态组件
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
