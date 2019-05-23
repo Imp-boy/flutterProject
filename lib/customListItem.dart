5fa59eb799d8093f9ac427bd2c4f096d2a58c18e
flutter <1126434179@qq.com>
2019-05-23 22:01:32 +0800

    尝试自定义组件

---
+++ lib/customListItem.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class customListItem extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Padding(
+      padding: new EdgeInsets.symmetric(vertical: 5),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            flex: 2,
+            child: ,
+          )
+        ],
+      ),
+    );
+  }
+}
