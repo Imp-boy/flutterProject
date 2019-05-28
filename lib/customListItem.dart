bb9cde1be3a53ba57a786c2d8a237d3743a544fe
flutter <1126434179@qq.com>
2019-05-28 22:50:56 +0800

    登录页布局

--- lib/customListItem.dart
+++ lib/customListItem.dart
@@ -1,17 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class customListItem extends StatelessWidget{
-  Widget build(BuildContext context){
-    return Padding(
-      padding: new EdgeInsets.symmetric(vertical: 5),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Expanded(
-            flex: 2,
-            child: null,
-          )
-        ],
+class CustomListItem extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return new Material(
+      color: Colors.lightBlue,
+      child: Center(
+        child: Container(
+            width: 200,
+            height: 200,
+            decoration: BoxDecoration(
+                color: Colors.blue, 
+                borderRadius: BorderRadius.circular(12)
+              ),
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text("用户登录"),
+                  Expanded(
+                      child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 60,
+                        alignment: Alignment.center,
+                        child: Text("用户名："),
+                      ),
+                      Expanded(
+                        child: TextField(),
+                      )
+                    ],
+                  )),
+                  Expanded(
+                      child: Row(
+                    children: <Widget>[
+                      Container(
+                        width: 60,
+                        alignment: Alignment.center,
+                        child: Text("密码："),
+                      ),
+                      Expanded(
+                        child: TextField(),
+                      )
+                    ],
+                  ))
+                ],
+              ),
+            )),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'list.dart';
+import 'customListItem.dart';
 void main() => runApp(new MyApp());
 //无状态组件
 class MyApp extends StatelessWidget {
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
         title: 'Startup Name Generator',
-        home: new RandomWords(),
+        home: new CustomListItem(),
         theme: new ThemeData(
             primaryColor: Colors.blue, primarySwatch: Colors.blue));
   }
