6f10fd1ee42cd08613928d704805e7540e2e8215
flutter <1126434179@qq.com>
2019-05-30 21:19:52 +0800

    删除没用的测试页

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'customListItem.dart';
+
 void main() => runApp(new MyApp());
+
 //无状态组件
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
         title: 'Startup Name Generator',
-        home: new CustomListItem(),
+        home: new Container(),
         theme: new ThemeData(
-            primaryColor: Colors.blue, primarySwatch: Colors.blue));
+          primaryColor: Colors.blue, primarySwatch: Colors.blue)
+        );
   }
 }
